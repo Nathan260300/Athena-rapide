8355aa328c1d5e86c4adb9107b409d68e4de440b
Nathan <nathan162318@gmail.com>
2025-10-06 21:30:26 +0200

    Delete js directory

--- js/redirigerversathéna.js
+++
@@ -1,9 +0,0 @@
-function redirigerVersSite() {
-            window.location.href = 'https://athena.moncollege-valdoise.fr/';
-        }
-
-        document.addEventListener('keydown', function(event) {
-            if (event.key === 'Enter') {
-                redirigerVersSite();
-            }
-        });
