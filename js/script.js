b8f629c998f30657a434ed5b4d54f41513d3d660
Nathan <nathan162318@gmail.com>
2025-10-21 22:25:55 +0200

    MESSAGES CONSOLES

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ document.addEventListener('keydown', function(event) {
         redirigerVersSite();
     }
 });
-
-console.log(
-   `%cMade with 🕑 and 💖 by Nathan J. – Last update : 06/10/2025`,
-   "background: #c3e88d; color: #233238; padding: .5em 1em;"
- )
+console.log("%c© 2025 - Nathan The Coder", "background: #282c34; color: #98c379; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cFast Athéna", "background: #282c34; color: #61afef; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cAccès rapide vers le Portail des Collégiens du Val d'Oise.","background: #282c34; color: #61dafb; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%chttps://athena-valdoise.netlify.app", "background: #282c34; color: #e06c75; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cMade with 🕑 and 💖 by Nathan J. – Last update : 21/10/2025","background: #282c34; color: #c678dd; padding: .5em 1em; border-radius: 5px; font-weight: bold;")
